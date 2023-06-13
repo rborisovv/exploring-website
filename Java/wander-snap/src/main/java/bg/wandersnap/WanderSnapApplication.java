c38192ac3c6a7d0fc309bb4c9cf7321de38208e0
Radoslav <radii2000@abv.bg>
2023-06-13 21:32:09 +0300

    Initial commit

---
+++ Java/wander-snap/src/main/java/bg/wandersnap/WanderSnapApplication.java
@@ -0,0 +1,12 @@
+package bg.wandersnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderSnapApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(WanderSnapApplication.class, args);
+	}
+}
---
+++ Java/wander-snap/src/test/java/bg/wandersnap/WanderSnapApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.wandersnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderSnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
