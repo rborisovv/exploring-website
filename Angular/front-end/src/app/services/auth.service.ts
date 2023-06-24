947daaec42cd429ff4a6c2a7011c0eeb5b997903
Radoslav <radii2000@abv.bg>
2023-06-24 09:50:22 +0300

    Added login service and model

---
+++ Angular/front-end/src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { UserLoginModel } from "../model/auth/UserLoginModel";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private API_URL: string = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public loginUser(loginModel: UserLoginModel): Observable<UserLoginModel> {
+    return this.http.post<UserLoginModel>(this.API_URL, loginModel);
+  }
+}
