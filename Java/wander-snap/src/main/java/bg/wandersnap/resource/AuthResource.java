b8c482fcf2be5331277dbb7724de7463a70453ec
Radoslav <radii2000@abv.bg>
2023-06-30 22:45:56 +0300

    Code cleanup

--- Java/wander-snap/src/main/java/bg/wandersnap/resource/AuthResource.java
+++ Java/wander-snap/src/main/java/bg/wandersnap/resource/AuthResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 public class AuthResource {
+
     @GetMapping("/csrf")
     public void obtainCsrfToken() {
 
