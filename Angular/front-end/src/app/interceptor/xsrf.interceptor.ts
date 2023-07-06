cb87acaefdf45f0d89f4cdba5c1dc1b2e198bda9
Radoslav <radii2000@abv.bg>
2023-07-06 22:00:37 +0300

    Changed Access token header name

--- Angular/front-end/src/app/interceptor/xsrf.interceptor.ts
+++ Angular/front-end/src/app/interceptor/xsrf.interceptor.ts
@@ -4,7 +4,7 @@ import { Observable } from "rxjs";
 
 @Injectable()
 export class XsrfInterceptor implements HttpInterceptor {
-  private static readonly jwtToken: string = 'JWT-TOKEN';
+  private static readonly accessToken: string = 'X-Access-Token';
 
   intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const csrfToken: string = 'XSRF-TOKEN';
@@ -12,14 +12,14 @@ export class XsrfInterceptor implements HttpInterceptor {
     return next.handle(httpRequest.clone({
       setHeaders: {
         'X-XSRF-TOKEN': XsrfInterceptor.obtainCsrfHeader(csrfToken),
-        'JWT-TOKEN': XsrfInterceptor.obtainJwtHeader()
+        'X-Access-Token': XsrfInterceptor.obtainJwtHeader()
       }
     }));
   }
 
   private static obtainJwtHeader(): string {
     const value: string = `; ${document.cookie}`;
-    const parts: string[] = value.split(`; ${this.jwtToken}=`);
+    const parts: string[] = value.split(`; ${this.accessToken}=`);
     return 'Bearer ' + parts.pop()?.split(';').shift();
   }
 
