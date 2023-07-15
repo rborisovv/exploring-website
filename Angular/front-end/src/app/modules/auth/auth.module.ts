bada66ee1b24c6b56d44f5634c83ddaa970b812d
Radoslav <radii2000@abv.bg>
2023-07-15 22:07:22 +0300

    Cookie service added

--- Angular/front-end/src/app/modules/auth/auth.module.ts
+++ Angular/front-end/src/app/modules/auth/auth.module.ts
@@ -5,11 +5,18 @@ import { RouterModule, Routes } from "@angular/router";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { ReactiveFormsModule } from "@angular/forms";
 import { TranslateModule } from "@ngx-translate/core";
+import { authGuard } from "../../guard/auth.guard";
+import { CookieService } from "ngx-cookie-service";
 
 const ROUTES: Routes = [
   {
     path: '', children: [
-      { path: 'login', title: 'WanderSnap | Login', component: LoginComponent }
+      {
+        path: 'login',
+        title: 'WanderSnap | Login',
+        component: LoginComponent,
+        canActivate: [authGuard]
+      }
     ]
   }
 ];
@@ -18,14 +25,14 @@ const ROUTES: Routes = [
   declarations: [
     LoginComponent
   ],
-    imports: [
-        CommonModule,
-        RouterModule.forChild(ROUTES),
-        FontAwesomeModule,
-        ReactiveFormsModule,
-        TranslateModule
-    ],
-  providers: []
+  imports: [
+    CommonModule,
+    RouterModule.forChild(ROUTES),
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    TranslateModule
+  ],
+  providers: [CookieService]
 })
 export class AuthModule {
 }
